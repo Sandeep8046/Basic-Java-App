fa360bd06dc316e5daa09c2cb93bab3428325746
sandeep8046 <sandeep23karanje@gmail.com>
2022-12-24 02:20:50 +0530

    Second line

--- Abc.java
+++ Abc.java
@@ -1,5 +1,7 @@
 class Abc {
     public static void main(String[] args) {
-        System.out.println("welcome to java!"); 
+        System.out.println("welcome to java!");
+        System.out.println("second line!"); 
+
     }
 }
